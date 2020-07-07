29b7f0d0c0ecd590a979b22c9d0d0c1d4e85e3f3
Natan Felles <natanfelles@gmail.com>
2020-07-07 19:28:01 -0300

    Add Contacts model and Contact::store

--- app/Controllers/Contact.php
+++ app/Controllers/Contact.php
@@ -1,5 +1,7 @@
 <?php namespace App\Controllers;
 
+use App\Models\Contacts;
+
 class Contact extends BaseController
 {
 	public function create()
@@ -12,5 +14,22 @@ class Contact extends BaseController
 
 	public function store()
 	{
+		$data = $this->request->getPost([
+			'name',
+			'email',
+			'subject',
+			'message',
+		]);
+		$contacts = new Contacts();
+		$id = $contacts->insert($data);
+		if ($id === false) {
+			return redirect()->route('contact.create')
+				->withInput()
+				->with('errors', $contacts->errors());
+		}
+		return redirect()->route('contact.create')->with('msg', [
+			'type' => 'success',
+			'contents' => 'Contact successful sent.',
+		]);
 	}
 }
---
+++ app/Models/Contacts.php
@@ -0,0 +1,46 @@
+<?php namespace App\Models;
+
+use CodeIgniter\Database\ConnectionInterface;
+use CodeIgniter\Model;
+use CodeIgniter\Validation\ValidationInterface;
+
+class Contacts extends Model
+{
+	protected $table = 'contacts';
+	protected $allowedFields = [
+		'name',
+		'email',
+		'subject',
+		'message',
+	];
+	protected $validationRules = [
+		'name' => [
+			'label' => 'Name',
+			'rules' => 'required|min_length[5]|max_length[64]',
+		],
+		'email' => [
+			'label' => 'Email',
+			'rules' => 'required|email',
+		],
+		'subject' => [
+			'label' => 'Subject',
+			'rules' => 'required|min_length[10]|max_length[255]',
+		],
+		'message' => [
+			'label' => 'Message',
+			'rules' => 'required|min_length[10]|max_length[2048]',
+		],
+	];
+
+	public function __construct(
+		ConnectionInterface &$db = null,
+		ValidationInterface $validation = null
+	) {
+		parent::__construct($db, $validation);
+
+		$this->validationRules['name']['label'] = lang('contact.name');
+		$this->validationRules['email']['label'] = lang('contact.email');
+		$this->validationRules['subject']['label'] = lang('contact.subject');
+		$this->validationRules['message']['label'] = lang('contact.message');
+	}
+}
